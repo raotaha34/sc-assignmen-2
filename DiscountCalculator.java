16c549cb4ebd064164bcd31d7f7029bd1224c7e3
raotaha34 <raotaha34@gmail.com>
2025-12-03 23:43:18 +0500

    Create DiscountCalculator class with methods

---
+++ DiscountCalculator.java
@@ -0,0 +1,7 @@
+public class Discountcalculator{
+  private map<UserTypr , double>discountRates;
+  public DiscountClalculator();
+  public double apllydiscount(UserType userType , double amount);
+  public void setDiscountRate(UserType usertype, double rate);
+  public double verifyDiscount(int userTypeCode,double bill)
+}
