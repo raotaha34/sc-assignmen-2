17e240a9bfd95f1ca14fd6cfcfd7cc86b01867fd
raotaha34 <raotaha34@gmail.com>
2025-12-03 23:36:16 +0500

    Add OrderResult class with fields and constructor

---
+++ OrderResult.java
@@ -0,0 +1,11 @@
+
+public class OrderResult {
+private OrderDetails orderDetails; private double subtotal;
+private double discount; private int shipping Cost; private double total; private boolean success;
+private  LocalDate Time  processed At;
+
+public OrderResult( OrderDetails orderDetails ,
+double subtotal , double discount , int shippingCost , double total , boolean success);
+
+// Getters for all fields
+}
