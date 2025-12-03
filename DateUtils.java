4c33ccff62eebac0111bf36c800a84cd79742bc3
raotaha34 <raotaha34@gmail.com>
2025-12-03 23:43:41 +0500

    Create DateUtils.java

---
+++ DateUtils.java
@@ -0,0 +1,6 @@
+public class Date Utils {
+public static final Date Time Formatter DEFAULT_FORMATTER = Date Time Formatter . ofPattern (" dd/ MM/ yyyy");
+
+public static String formatDate ( LocalDate date ); public static LocalDate parse Date ( String date String );
+public static boolean isValid Date ( int day , int month , int year);
+}
